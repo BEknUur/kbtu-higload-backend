f6631d1727739435b0187a9d3f908185be83ada4
Beknur <ualihanulybeknur@gmail.com>
2026-09-17 11:14:22 +0500

    add prometheus and grafana to docker-compose

--- practice-2/service/app/main.py
+++ practice-2/service/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI 
-
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     title ="Higload-Backend",
     version="1.0.0",
 )
 
+Instrumentator().instrument(app).expose(app)
 
 
 
